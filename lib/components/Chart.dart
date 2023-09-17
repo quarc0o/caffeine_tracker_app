52900f15b2d1924133efd6daf9d92c7b89500654
quarc0o <emmanuelq150@gmail.com>
2023-09-17 21:07:49 +0200

    chart looking decent

--- lib/components/Chart.dart
+++ lib/components/Chart.dart
@@ -1,10 +1,18 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<double> caffeineValues;
 
   const Chart({Key? key, required this.caffeineValues}) : super(key: key);
+
+  String getHourString(int index) {
+    final now = DateTime.now();
+    final targetTime =
+        now.add(Duration(hours: index * 2)); // adding 2 hours for each step
+    return DateFormat('HH:mm').format(targetTime);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,107 +28,111 @@ class Chart extends StatelessWidget {
     if (intervalValue == 0) intervalValue = 1;
 
     return Container(
-      width: 310,
-      height: 210,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 4),
-            blurRadius: 4,
-            spreadRadius: 1,
-          ),
-        ],
-      ),
-      padding: const EdgeInsets.all(5),
-      child: SizedBox(
-        width: 300,
-        height: 200,
-        child: LineChart(
-          LineChartData(
-            backgroundColor: Colors.white,
-            gridData: FlGridData(show: false),
-            titlesData: FlTitlesData(
-              show: true,
-              rightTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
-              ),
-              topTitles: AxisTitles(
-                sideTitles: SideTitles(showTitles: false),
-              ),
+        width: 330,
+        height: 210,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 4),
+              blurRadius: 4,
+              spreadRadius: 1,
             ),
-            minX: 0,
-            maxX: caffeineValues.length.toDouble() - 1,
-            minY: 0,
-            maxY: caffeineValues.reduce(
-                    (value, element) => value > element ? value : element) +
-                10, // Setting maximum Y to a bit more than the max caffeine value for better chart visibility.
-            borderData: FlBorderData(show: false),
-            lineBarsData: [
-              LineChartBarData(
-                spots: caffeineData,
-                isCurved: true,
-                barWidth: 6,
-                isStrokeCapRound: true,
-                belowBarData: BarAreaData(show: false),
-                dotData: FlDotData(show: false),
-              ),
-            ],
-          ),
+          ],
         ),
-      ),
-    );
+        padding: const EdgeInsets.all(5),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: LineChart(
+            LineChartData(
+              clipData: FlClipData.all(),
+              backgroundColor: Colors.white,
+              gridData: FlGridData(show: false),
+              titlesData: FlTitlesData(
+                show: true,
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                topTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false),
+                ),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: bottomTitleWidgets,
+                    reservedSize: 30,
+                  ),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: leftTitleWidgets,
+                    reservedSize: 50,
+                  ),
+                ),
+              ),
+              minX: 0,
+              maxX: caffeineValues.length.toDouble() - 1,
+              minY: 0,
+              maxY: caffeineValues.reduce(
+                      (value, element) => value > element ? value : element) +
+                  10, // Setting maximum Y to a bit more than the max caffeine value for better chart visibility.
+              borderData: FlBorderData(show: false),
+              lineBarsData: [
+                LineChartBarData(
+                  spots: caffeineData,
+                  isCurved: true,
+                  barWidth: 6,
+                  isStrokeCapRound: true,
+                  belowBarData: BarAreaData(show: false),
+                  dotData: FlDotData(show: false),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
 
 Widget bottomTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
+  final style = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 10,
   );
-  Widget text;
-  switch (value.toInt()) {
-    case 2:
-      text = const Text('MAR', style: style);
-      break;
-    case 5:
-      text = const Text('JUN', style: style);
-      break;
-    case 8:
-      text = const Text('SEP', style: style);
-      break;
-    default:
-      text = const Text('', style: style);
-      break;
+  String getHourString(int index) {
+    final now = DateTime.now();
+    final targetTime =
+        now.add(Duration(hours: index * 2)); // adding 2 hours for each step
+    return DateFormat('HH:mm').format(targetTime);
   }
+
+  String text = getHourString(value.toInt());
 
   return SideTitleWidget(
     axisSide: meta.axisSide,
-    child: text,
+    child: Text(text, style: style),
   );
 }
 
 Widget leftTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
+  final style = TextStyle(
     fontWeight: FontWeight.bold,
-    fontSize: 15,
+    fontSize: 10,
   );
+
+  // Format the Y-value as a string
   String text;
-  switch (value.toInt()) {
-    case 1:
-      text = '10K';
-      break;
-    case 3:
-      text = '30k';
-      break;
-    case 5:
-      text = '50k';
-      break;
-    default:
-      return Container();
+  if (value % 1 == 0) {
+    // Check if the value is an integer
+    text = '${value.toInt()} mg'; // Convert to integer and add 'K' postfix
+  } else {
+    text = '${value.toStringAsFixed(1)} mg'; // Keep one decimal point if needed
   }
 
-  return Text(text, style: style, textAlign: TextAlign.left);
+  return SideTitleWidget(
+    axisSide: meta.axisSide,
+    child: Text(text, style: style),
+  );
 }
