eec9b7235f0d92e9a3357d5a9c85bd8174039038
quarc0o <emmanuelq150@gmail.com>
2023-09-18 02:54:21 +0200

    barcode scanner working

--- lib/views/AddProductScreen.dart
+++ lib/views/AddProductScreen.dart
@@ -1,3 +1,6 @@
+import 'package:barcode_scanner/barcode_scanning_data.dart';
+import 'package:barcode_scanner/json/common_data.dart';
+import 'package:barcode_scanner/scanbot_barcode_sdk.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
@@ -35,6 +38,43 @@ class _AddProductScreenState extends State<AddProductScreen> {
     return caffeineAmountPerCup * _numberOfCups;
   }
 
+  // 2. Introduce function to invoke the barcode scanner
+  _scanBarcode({bool shouldSnapImage = false}) async {
+    final additionalParameters = BarcodeAdditionalParameters(
+      minimumTextLength: 3,
+      maximumTextLength: 45,
+      minimum1DBarcodesQuietZone: 10,
+      codeDensity: CodeDensity.HIGH,
+    );
+    var config = BarcodeScannerConfiguration(
+      barcodeImageGenerationType: shouldSnapImage
+          ? BarcodeImageGenerationType.VIDEO_FRAME
+          : BarcodeImageGenerationType.NONE,
+      topBarBackgroundColor: Colors.blueAccent,
+      finderLineColor: Colors.red,
+      cancelButtonTitle: "Cancel",
+      finderTextHint:
+          "Please align any supported barcode in the frame to scan it.",
+      successBeepEnabled: true,
+
+      // cameraZoomFactor: 1,
+      additionalParameters: additionalParameters,
+      // see further customization configs ...
+      orientationLockMode: OrientationLockMode.NONE,
+      //useButtonsAllCaps: true,
+    );
+
+    try {
+      var result = await ScanbotBarcodeSdk.startBarcodeScanner(config);
+
+      if (result.operationResult == OperationResult.SUCCESS) {
+        // TODO handle result
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     UserProvider userProvider =
@@ -62,6 +102,11 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 );
               }).toList(),
             ),
+            if (_selectedProduct == "energy drink")
+              ElevatedButton(
+                onPressed: _scanBarcode,
+                child: Text("Scan Barcode"),
+              ),
             Expanded(
               child: Center(
                 child: Image.asset(
